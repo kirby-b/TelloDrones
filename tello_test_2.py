d9d1dd082c3478b3b7ad1dc5b5c912d43626e3a5
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-08 11:43:18 -0500

    Update tello_test_2.py

--- tello_test_2.py
+++ tello_test_2.py
@@ -19,3 +19,7 @@ sleep(8)
 # flips the drone left, right, backward, then forward
 drone.land()
 # lands
+drone.turn_motor_on()
+sleep(10)
+drone.turn_motor_off()
+# Turns the cooling motor on for 10 seconds
