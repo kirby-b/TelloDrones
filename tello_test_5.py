a4ed946e94afe93f43c916d3769ab779dc6449fe
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-08 11:50:24 -0500

    Update tello_test_5.py

--- tello_test_5.py
+++ tello_test_5.py
@@ -1,9 +1,28 @@
 from djitellopy import tello
+from time import sleep
 
 drone = tello.Tello()
 
 drone.connect()
-# Connects to the drone
-
-drone.query_battery()
-# gets the battery percent
+sleep(1)
+drone.takeoff()
+sleep(4)
+# connects and takes off
+for i in range(5):
+  drone.flip("l")
+  sleep(8)
+  drone.rotate_clockwise(360)
+  sleep(8)
+# flips the drone left and does a 360 clockwise 5 times
+for i in range(5):
+  drone.flip("r")
+  sleep(8)
+  drone.rotate_counter_clockwise(360)
+  sleep(8)
+# flips the drone left and does a 360 counter-clockwise 5 times
+drone.land()
+# lands
+drone.turn_motor_on()
+sleep(30)
+drone.turn_motor_off()
+# Turns the cooling motor on for 30 seconds
