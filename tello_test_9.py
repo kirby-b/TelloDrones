14b843c66c848432bcd215028be77e79d3463d2a
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-08 11:24:52 -0500

    Create tello_test_9.py

---
+++ tello_test_9.py
@@ -0,0 +1,25 @@
+# This will be the same as 7 but I set the speed higher
+from djitellopy import tello
+from time import sleep
+
+drone = tello.Tello()
+
+drone.connect()
+sleep(1)
+drone.takeoff()
+sleep(4)
+# connects and takes off
+drone.set_speed(50)
+sleep(8)
+drone.move_up(50)
+sleep(8)
+drone.move_forward(100)
+sleep(8)
+drone.move_down(50)
+sleep(8)
+drone.move_back(100)
+sleep(8)
+drone.rotate_clockwise(360)
+# moves up, down, and rotates 360
+drone.land()
+# lands
