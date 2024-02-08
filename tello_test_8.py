e321e04dc11002623b16899eb20e560f9cceba3a
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-08 11:21:28 -0500

    Create tello_test_8.py

---
+++ tello_test_8.py
@@ -0,0 +1,36 @@
+from djitellopy import tello
+from time import sleep
+
+drone = tello.Tello()
+
+drone.connect()
+sleep(1)
+drone.takeoff()
+sleep(4)
+# connects and takes off
+drone.move_up(60)
+sleep(8)
+drone.move_down(60)
+sleep(8)
+# Moves up and down
+drone.rotate_counter_clockwise(90)
+sleep(8)
+drone.rotate_clockwise(90)
+sleep(8)
+# Rotates back and forth
+drone.move_forward(100)
+sleep(8)
+drone.move_back(100)
+sleep(8)
+drone.move_left(100)
+sleep(8)
+drone.move_right(100)
+sleep(8)
+# Moves back, forward, left, right
+drone.rotate_clockwise(360)
+sleep(8)
+drone.rotate_counter_clockwise(360)
+sleep(8)
+# Rotates 360 in both directions
+drone.land()
+# lands
