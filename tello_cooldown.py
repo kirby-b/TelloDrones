883135eecec37fdf42683092b4bce3dc98fc46f8
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-08 11:39:47 -0500

    Create tello_cooldown.py

---
+++ tello_cooldown.py
@@ -0,0 +1,12 @@
+from djitellopy import tello
+from time import sleep
+
+drone = tello.Tello()
+
+drone.connect()
+sleep(1)
+# connects
+drone.turn_motor_on()
+sleep(30)
+drone.turn_motor_off()
+# Turns the colling motor on for 30 seconds
