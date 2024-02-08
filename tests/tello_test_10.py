81fdcec23924c8a17610ef0fa93ec8644099cb25
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-08 13:35:50 -0500

    Create tello_test_10.py

---
+++ tests/tello_test_10.py
@@ -0,0 +1,43 @@
+# Faster version of 5 wth some extras
+from djitellopy import tello
+from time import sleep
+
+drone = tello.Tello()
+
+def main():
+  
+  drone.connect()
+  sleep(1)
+  drone.takeoff()
+  sleep(4)
+  # connects and takes off
+  drone.set_speed(60)
+  sleep(5)
+  # Sets speed
+  for i in range(5):
+    drone.flip("l")
+    sleep(8)
+    drone.rotate_clockwise(360)
+    sleep(8)
+    square()
+  # flips the drone left and does a 360 clockwise 5 times
+  for i in range(5):
+    drone.flip("r")
+    sleep(8)
+    drone.rotate_counter_clockwise(360)
+    sleep(8)
+    square()
+  # flips the drone left and does a 360 counter-clockwise 5 times
+  drone.land()
+  # lands
+  drone.turn_motor_on()
+  sleep(30)
+  drone.turn_motor_off()
+  # Turns the cooling motor on for 30 seconds
+def square():
+  for i in range(4):
+    drone.rotate_clockwise(90)
+    sleep(8)
+    drone.move_forward(100)
+    sleep(8)
+  # Flys in a square by turning 90 degrees and moving forward 4 times
