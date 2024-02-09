e855b1b06830ad7b791e1b5b2482a5ce38d6fa37
Kirby-b <kirby.brodie66@gmail.com>
2024-02-09 08:27:47 -0500

    Update tello_test_2.py

--- tests/tello_test_2.py
+++ tests/tello_test_2.py
@@ -5,17 +5,21 @@ drone = tello.Tello()
 
 drone.connect()
 sleep(1)
+# connects
+drone.query_battery()
+# gets the battery percent
+sleep(1)
 drone.takeoff()
 sleep(4)
-# connects and takes off
+# takes off
 drone.flip("l")
-sleep(8)
+sleep(1)
 drone.flip("r")
-sleep(8)
+sleep(1)
 drone.flip("f")
-sleep(8)
+sleep(1)
 drone.flip("b")
-sleep(8)
+sleep(5)
 # flips the drone left, right, backward, then forward
 drone.land()
 # lands
