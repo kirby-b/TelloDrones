cec2679e072348b6d9c797325eca9c79085f2540
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 15:34:26 -0500

    Update tello_test_10.py

--- tests/tello_test_10.py
+++ tests/tello_test_10.py
@@ -4,44 +4,44 @@ from time import sleep
 
 drone = tello.Tello()
 
+
 def main():
-  
-  drone.connect()
-  sleep(1)
-  # connects
-  drone.query_battery()
-  # gets the battery percent
-  sleep(1)
-  drone.takeoff()
-  sleep(4)
-  # takes off
-  drone.set_speed(60)
-  sleep(5)
-  # Sets speed
-  for i in range(5):
-    drone.flip("l")
+    drone.connect()
+    sleep(1)
+    # connects
+    drone.query_battery()
+    # gets the battery percent
+    sleep(1)
+    drone.takeoff()
+    sleep(4)
+    # takes off
+    drone.set_speed(60)
     sleep(5)
-    drone.rotate_clockwise(360)
-    sleep(5)
-    square()
-  # flips the drone left and does a 360 clockwise 5 times
-  for i in range(5):
-    drone.flip("r")
-    sleep(5)
-    drone.rotate_counter_clockwise(360)
-    sleep(5)
-    square()
-  # flips the drone left and does a 360 counter-clockwise 5 times
-  drone.land()
-  # lands
-  drone.turn_motor_on()
-  sleep(30)
-  drone.turn_motor_off()
-  # Turns the cooling motor on for 30 seconds
+    # Sets speed
+    for i in range(5):
+        drone.flip("l")
+        sleep(5)
+        drone.rotate_clockwise(360)
+        sleep(5)
+        square()
+    # flips the drone left and does a 360 clockwise 5 times
+    for i in range(5):
+        drone.flip("r")
+        sleep(5)
+        drone.rotate_counter_clockwise(360)
+        sleep(5)
+        square()
+    # flips the drone left and does a 360 counter-clockwise 5 times
+    drone.land()
+    # lands
+    drone.turn_motor_on()
+    sleep(30)
+    drone.turn_motor_off()
+    # Turns the cooling motor on for 30 seconds
 def square():
-  for i in range(4):
-    drone.rotate_clockwise(90)
-    sleep(10)
-    drone.move_forward(100)
-    sleep(10)
-  # Flys in a square by turning 90 degrees and moving forward 4 times
+    for i in range(4):
+        drone.rotate_clockwise(90)
+        sleep(10)
+        drone.move_forward(100)
+        sleep(10)
+    # Flies in a square by turning 90 degrees and moving forward 4 times
