412ee2de352a5cc2b1e3d43dbf5e467ea627ee11
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-03-27 11:31:46 -0400

    Update tello_test_7.py
    -Removing this part until I figure out cooling

--- tests/tello_test_7.py
+++ tests/tello_test_7.py
@@ -24,7 +24,3 @@ drone.rotate_clockwise(360)
 # moves up, down, and rotates 360
 drone.land()
 # lands
-drone.turn_motor_on()
-sleep(10)
-drone.turn_motor_off()
-# Turns the cooling motor on for 10 seconds
