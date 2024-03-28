4e72bda176d169f69c3b5ae501af7d73a87ee9c8
Kirby-b <kirby.brodie66@gmail.com>
2024-03-28 10:09:07 -0400

    Create tello_test_cam.py
    - This was actually kinda fun

---
+++ tests/tello_test_cam.py
@@ -0,0 +1,25 @@
+import cv2
+from djitellopy import tello
+
+def show_camera_frames(drone):
+    # Shows the camera frames through cv2
+    while True:
+        frame = drone.get_frame_read().frame
+        cv2.imshow("Frame", frame)
+        if cv2.waitKey(1) and 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+    drone.end()
+
+def main():
+    # Initialize drone
+    drone = tello.Tello()
+    # Connect to drone
+    drone.connect()
+    # Turn on drone cam
+    drone.streamon()
+    # Call the cv camera function
+    show_camera_frames(drone)
+
+if __name__ == "__main__":
+    main()
