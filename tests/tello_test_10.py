8cdd3824deb732ffe7e1eed80adf80d13ebbf65e
Kirby-b <kirby.brodie66@gmail.com>
2024-03-28 10:08:11 -0400

    Update tello_test_10.py

--- tests/tello_test_10.py
+++ tests/tello_test_10.py
@@ -20,24 +20,28 @@ def main():
     # Sets speed
     for i in range(5):
         drone.flip("l")
-        sleep(7)
+        sleep(6)
         drone.rotate_clockwise(360)
         sleep(5)
         square()
     # flips the drone left and does a 360 clockwise 5 times
     for i in range(5):
         drone.flip("r")
-        sleep(7)
+        sleep(6)
         drone.rotate_counter_clockwise(360)
         sleep(5)
         square()
     # flips the drone left and does a 360 counter-clockwise 5 times
     drone.land()
     # lands
+    drone.end()
 def square():
     for i in range(4):
         drone.rotate_clockwise(90)
-        sleep(10)
+        sleep(6)
         drone.move_forward(100)
-        sleep(10)
+        sleep(6)
     # Flies in a square by turning 90 degrees and moving forward 4 times
+
+if __name__ == "__main__":
+    main()
