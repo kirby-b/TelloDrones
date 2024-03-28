dd2648b6f13a313f5cd88339dc487f2aee342853
Kirby-b <kirby.brodie66@gmail.com>
2024-03-28 10:08:50 -0400

    Update tello_time_test.py

--- tests/tello_time_test.py
+++ tests/tello_time_test.py
@@ -13,9 +13,11 @@ drone.takeoff()
 sleep(4)
 # takes off
 drone.move_up(100)
+sleep(6)
 for i in range(6):
-    sleep(10)
+    sleep(7)
     drone.get_flight_time()
 # gets the flight time every 10 seconds for 60 seconds
 drone.land()
 # lands
+drone.end()
