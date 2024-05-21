3bcde0c083d5dab6b8611feb00d8134bc6b5d09a
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-05-21 08:18:11 -0400

    Update fly_with_cam.py
    Draft 2. I replaced the movement with something that is less error prone and made the drone object global.
    I also had to delete the extra show frames call and move the drone end

--- tests/fly_with_cam.py
+++ tests/fly_with_cam.py
@@ -4,8 +4,10 @@ from djitellopy import tello
 from time import sleep
 import threading
 
+# Initialize drone
+drone = tello.Tello()
 
-def show_camera_frames(drone):
+def show_camera_frames():
     # Shows the camera frames through cv2
     while True:
         frame = drone.get_frame_read().frame
@@ -13,43 +15,39 @@ def show_camera_frames(drone):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cv2.destroyAllWindows()
-    drone.end()
 
-def movement(drone):
+def movement():
     sleep(1)
-    drone.rotate_counter_clockwise(90)
-    sleep(6)
-    drone.rotate_clockwise(90)
-    sleep(6)
-    # rotates back and forth
+    drone.move_up(60)
+    sleep(7)
+    drone.move_down(60)
+    sleep(7)
+    # moves up then down
     drone.land()
     # lands
-    drone.end()
 
 
 def main():
-    # Initialize drone
-    drone = tello.Tello()
     # Connect to drone
     drone.connect()
     # Turn on drone cam
-    drone.query_battery()
-    # gets the battery percent
     sleep(1)
     drone.takeoff()
     sleep(3)
     # Takes off
     drone.streamon()
     # Call the cv camera function
-    show_camera_frames(drone)
-    t1 = threading.Thread(target=show_camera_frames, args=(drone))
-    t2 = threading.Thread(target=movement, args=(drone))
+    t1 = threading.Thread(target=show_camera_frames, args=())
+    # Makes the drone move
+    t2 = threading.Thread(target=movement, args=())
  
     t1.start()
     t2.start()
  
     t1.join()
     t2.join()
+    
+    drone.end()
 
 
 if __name__ == "__main__":
