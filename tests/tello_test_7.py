16e414576bca92e1f6ccf8e7b2796069ffe25a44
kirby-b <kirby.brodie66@gmail.com>
2024-12-05 11:09:55 -0500

    Update tello_test_7.py

--- tests/tello_test_7.py
+++ tests/tello_test_7.py
@@ -6,7 +6,7 @@ drone = tello.Tello()
 
 drone.connect()
 sleep(1)
-# connects
+# connects and sleeps to ensure it is ready
 sleep(1)
 drone.takeoff()
 sleep(4)
@@ -24,6 +24,7 @@ sleep(6)
 drone.rotate_counter_clockwise(360)
 sleep(6)
 # moves up, down, and rotates 360 in both direction
+# It sleeps in between each so the drone doesnt error
 drone.land()
 # lands
 drone.end()
