842b923092b25610b9f7f23c9c81489b0b107c70
kirby-b <kirby.brodie66@gmail.com>
2024-12-05 11:09:50 -0500

    Update tello_test_5.py

--- tests/tello_test_5.py
+++ tests/tello_test_5.py
@@ -6,23 +6,29 @@ drone = tello.Tello()
 
 drone.connect()
 sleep(1)
-# connects
+# connects and sleeps to ensure it is ready
 sleep(1)
 drone.takeoff()
 sleep(4)
 # takes off
+
+# I decided to make the two movements be in loops
+# so this isnt as long and to keep best practice
+
 for i in range(5):
     drone.flip("l")
     sleep(6)
     drone.rotate_clockwise(360)
     sleep(7)
 # flips the drone left and does a 360 clockwise 5 times
+# It sleeps in between each so the drone doesnt error
 for i in range(5):
     drone.flip("r")
     sleep(6)
     drone.rotate_counter_clockwise(360)
     sleep(7)
 # flips the drone left and does a 360 counter-clockwise 5 times
+# It sleeps in between each so the drone doesnt error
 drone.land()
 # lands
 drone.end()
