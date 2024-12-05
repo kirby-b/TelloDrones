3bc818e0df3db38725967355eff6e07f31e84293
kirby-b <kirby.brodie66@gmail.com>
2024-12-05 11:10:04 -0500

    Update tello_time_test.py

--- tests/tello_time_test.py
+++ tests/tello_time_test.py
@@ -6,16 +6,16 @@ drone = tello.Tello()
 
 drone.connect()
 sleep(1)
-# connects
+# connects and sleeps to ensure it is ready
 
 sleep(1)
 drone.takeoff()
 sleep(4)
-# takes off
 drone.move_up(100)
 sleep(6)
+# The drone takes off and moves up to make sure the drone is considered flying
 for i in range(6):
-    sleep(7)
+    sleep(10)
     drone.get_flight_time()
 # gets the flight time every 10 seconds for 60 seconds
 drone.land()
